7865ede2d9e0da15e4b07f7513d0ac7b81f4ad16
Mahamudul <mahamudul0113@gmail.com>
2021-02-02 02:30:54 +0600

    arithmetic operations

--- helloworld/helloworld/Program.cs
+++ helloworld/helloworld/Program.cs
@@ -11,6 +11,14 @@ namespace helloworld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+
+
+            int num1 = 5;
+            int num2 = 3;
+            Console.WriteLine("Addition is :" + (num1 + num2));
+            Console.WriteLine("Substraction is :{0}", (num1 - num2));
+            Console.WriteLine("Multiplication is :{0}", (num1 * num2));
+            Console.WriteLine("Division is :" + (num1 / num2));
         }
     }
 }
