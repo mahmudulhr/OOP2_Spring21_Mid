91166ac7075fff3896bf964aa8be0f0b86bd83d8
Mahamudul <mahamudul0113@gmail.com>
2021-02-07 12:50:59 +0600

    even odd

--- helloworld/helloworld/Program.cs
+++ helloworld/helloworld/Program.cs
@@ -20,17 +20,20 @@ namespace helloworld
                     Console.WriteLine(i+" ");
                 }
             }
-            /*/
-            int n= 99;
-            Console.WriteLine("odd");
-            for(int i=1; i<=n;i++)
-            {
-                if (i%2 !=0)
-                {
-                    Console.WriteLine(i + "");
-                }
+            
+          /*/
+            int i, n =100 , sum1 = 0, sum2 = 0;
+            Console.WriteLine("enter value");
+            for (i=1; i<= n; i++)
+            { if (i % 2 == 1)
+                    sum1 = sum1 + 1;
+                else
+                    sum2 = sum2 + 1;
+                Console.WriteLine("Sum of odd ={0}", sum1);
+                Console.WriteLine("Sum of even ={0}", sum2);
+
             }
-          
+            
         }
        
     }
