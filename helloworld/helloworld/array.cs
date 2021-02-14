648109db53c2f4a94e7220b5cf471f0dd0176fed
Mahamudul <mahamudul0113@gmail.com>
2021-02-14 13:59:05 +0600

    array

---
+++ helloworld/helloworld/array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    class array
+    {
+        static void Main(string[] args)
+        {
+            int arr1 = new int[n];
+            int arr2 = new int[n];
+            for{
+                for(int i =0; i<arr1.length;i++)
+                {
+                    Console.WriteLine(arr[i] = i + n);
+                }
+            }
+
+
+        }
+    }
+}
